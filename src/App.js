bed80e09651dcefcba964174a17ee09f4338347f
vinodkotagiri <vinodkotagiri@icloud.com>
2022-06-19 21:44:19 +0530

    Components are updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Component} from "react";
+import CardList from "./cards-list/card-list.component";
+import SearchBox from "./search-box/search-box.component";
+class App extends Component {
+    constructor(){
+        super();
+        this.state={
+            monsters:[],
+            searchField:'',
+        };
+    }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=>response.json())
+        .then((users)=>this.setState(
+            ()=>{
+            return {monsters:users}
+            }
+       ));
+    }
+    onSearchChange=(event)=>{
+        const searchField=event.target.value.toLocaleLowerCase();
+        this.setState(()=>{
+            return {searchField};
+        });
+    }
+    render(){
+        const {monsters, searchField}=this.state;
+        const {onSearchChange}=this;
+        const filteredMonsters=monsters.filter((monster)=>{
+            return monster.name.toLocaleLowerCase().includes(searchField);
+        });
+        return <div className="App">
+        <SearchBox onChangeHandler={onSearchChange} placeholder={'Search Monsters'} className={'search-box'}/>
+        <CardList monsters={filteredMonsters}/>
+        </div>
+    }
 }
-
 export default App;
---
+++ src/cards-list/card-list.component.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class CardList extends Component {
+    render() {
+        const {monsters}=this.props;
+        return <>
+            {monsters.map(monster=>{
+                return <h1 key={monster.id}>{monster.name}</h1>
+            })}
+        </>
+    }
+}
+export default CardList;
